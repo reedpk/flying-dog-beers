c8438888b846b2fa7e3a6e64eb248e188715ee3a
Patrick Reed <62704187+reedpk@users.noreply.github.com>
2020-05-24 11:34:17 -0400

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ app.layout = html.Div(children=[
     html.H1(myheading),
     html.Div([
         html.H6('Curling Club Map'),
-        html.Iframe(id='map',srcDoc=open('assets/FullCurlmap.html').read(),width='100%', height='100%')
+        html.Iframe(id='map',srcDoc=open('assets/FullCurlmap.html').read(),width='100%', height='800')
     ]),
     
     ]
